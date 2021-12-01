80b4e7903ffcfafcb0dff2eba2a8cb5710c34c33
hugofnm <hugo.meleiro@epita.fr>
2021-12-01 17:17:21 +0100

    vive epita

---
+++ LAB2/BruhProjectV2 TM/Assets/Script/Scene2Manager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class Scene2Manager : MonoBehaviour
+{
+    public ARRaycastM
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
